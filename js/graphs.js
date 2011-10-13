ee5c4aff56bbd4ffe9df75cf66d359fb29d5f393
Smotko <ensmotko@gmail.com>
2011-10-13 23:07:53 +0200

    Changed the way graphs look like, added legend

--- js/graphs.js
+++ js/graphs.js
@@ -1,13 +1,13 @@
 function drawGraph(){
 	
 	heat_losses = new Array(
-		   	 [[14,44,26],'heat losses']
+		   	 [[14,44,26,32,32],'heat losses']
 	);	
 
 	$('#graph_0').jqBarGraph({
 	  	 data: heat_losses,
-	  	 colors: ['#242424','#437346','#97D95C'] ,
-	   legends: ['ads','leads','google ads'],
+	  	 colors: ['#00ff00','#993300','#00ccff', '#ff0000', '#000080'] ,
+	   legends: ['transmission losses floor','transmission losses window','transmission losses wall', 'transmission losses roof', 'ventilation losses'],
 	   legend: true
 	});
 	
@@ -17,20 +17,20 @@ function drawGraph(){
 
 	$('#graph_1').jqBarGraph({
 	  	 data: heat_gains,
-	  	 colors: ['#242424','#437346','#97D95C'] ,
-	   legends: ['ads','leads','google ads'],
+	  	 colors: ['#ff9900','#008000','#ffff00'] ,
+	   legends: ['energy need for heating','usable internal heat load','usable solar heat load'],
 	   legend: true
 	});
 	
 	
 	energy_need = new Array(
-		   	 [[14,44,26],'heat gains']
+		   	 [[14,44],'heat gains']
 	);	
 
 	$('#graph_2').jqBarGraph({
 	  	 data: energy_need,
-	  	 colors: ['#242424','#437346','#97D95C'] ,
-	   legends: ['ads','leads','google ads'],
+	  	 colors: ['#ffcd9c','#ff9900'] ,
+	   legends: ['domestic hot water','space heating'],
 	   legend: true
 	});
 	
@@ -39,20 +39,20 @@ function drawGraph(){
 	);	
 
 	$('#graph_3').jqBarGraph({
-	  	 data: energy_need,
-	  	 colors: ['#242424','#437346','#97D95C'] ,
-	   legends: ['ads','leads','google ads'],
+	  	 data: energy_carriers,
+	  	 colors: ['#840909','#cce6ff','#078407', '#ffff06', '#151515', '#fefbf5'] ,
+	   legends: ['fossil fuels','distinct heating','bio mass', 'electricity', 'other', 'produced electricity'],
 	   legend: true
 	});
 	
 	primary_energy = new Array(
-		   	 [[14,44,26],'primary energy']
+		   	 [[14,44],'primary energy']
 	);	
 
 	$('#graph_4').jqBarGraph({
-	  	 data: energy_need,
-	  	 colors: ['#242424','#437346','#97D95C'] ,
-	   legends: ['ads','leads','google ads'],
+	  	 data: primary_energy,
+	  	 colors: ['#eaeaea','#808080'] ,
+	   legends: ['primary energy for hot water','primary energy for heating'],
 	   legend: true
 	});
 
