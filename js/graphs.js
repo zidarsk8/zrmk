f6ff5be16a4eabc35c00e842af50caf5e9244372
Smotko <ensmotko@gmail.com>
2011-10-13 22:03:42 +0200

    Graphs

--- js/graphs.js
+++ js/graphs.js
@@ -1,17 +1,12 @@
 function drawGraph(){
 	
-	arrayOfDataMulti = new Array(
+	arrayOfData = new Array(
 	    [[30,44,26],'2007'],
 	    [[18,38,38],'2008'],
 	    [[24,32,57],'2009']
 	);
-	
-	$('#graph_0').jqBarGraph({
-	     data: arrayOfDataMulti,
-	     colors: ['#242424','#437346','#97D95C'],
-		 width: 260, // default width of your graph
-		 height: 400 //default height of your graph
-	}); 
+	$('#divForGraph').jqBarGraph({ data: arrayOfData }); 
+
 	
 }
 
