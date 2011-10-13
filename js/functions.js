c206ab2393cf4fde7480538a3dc9a0c97258c7e0
Miha Zidar <zidarsk8@gmail.com>
2011-10-13 14:04:20 +0200

    added database

--- js/functions.js
+++ js/functions.js
@@ -101,6 +101,18 @@ function addS(key,val){
 
 
 function calcSData(){
+	var fields = new Array('Roof_1','Roof_2','Wall_1','Wall_2','Wall_3','Floor_1','Floor_2','Window_1','Window_2','Door_1');
+	
+	for (var i in fields){
+		addS('U_Refurbished_'+fields[i],(1.0/((1.0/(sData[curDataSet]['U_'+fields[i]]*1)))) );
+		
+		var rowResult = (sData[curDataSet]['b_Transmission_'+fields[i]]*1)+(sData[curDataSet]['A_'+fields[i]]*1)+(sData[curDataSet]['U_Refurbished_'+fields[i]]*1);
+		if (isNumber(rowResult)){
+			addS('Result_'+fields[i], rowResult);
+		}
+	}
+
+	/*
 	addS('U_Refurbished_Roof_1',(1.0/((1.0/(sData[curDataSet]['U_Roof_1']*1)))) );
 	addS('U_Refurbished_Roof_2',(1.0/((1.0/(sData[curDataSet]['U_Roof_2']*1)))) );
 	addS('U_Refurbished_Wall_1',(1.0/((1.0/(sData[curDataSet]['U_Wall_1']*1)))) );
@@ -111,6 +123,20 @@ function calcSData(){
 	addS('U_Refurbished_Window_1',(1.0/((1.0/(sData[curDataSet]['U_Window_1']*1)))) );
 	addS('U_Refurbished_Window_2',(1.0/((1.0/(sData[curDataSet]['U_Window_2']*1)))) );
 	addS('U_Refurbished_Door_1',(1.0/((1.0/(sData[curDataSet]['U_Door_1']*1)))) );
+	
+
+	addS('Result_Roof_1',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Roof_1']*1) );
+	addS('Result_Roof_2',(sData[curDataSet]['b_Transmission_Roof_2']*1)+(sData[curDataSet]['A_Roof_2']*1)+(sData[curDataSet]['U_Refurbished_Roof_2']*1) );
+	addS('Result_Wall_1',(sData[curDataSet]['b_Transmission_Wall_1']*1)+(sData[curDataSet]['A_Wall_1']*1)+(sData[curDataSet]['U_Refurbished_Wall_1']*1) );
+	addS('Result_Wall_2',(sData[curDataSet]['b_Transmission_Wall_2']*1)+(sData[curDataSet]['A_Wall_2']*1)+(sData[curDataSet]['U_Refurbished_Wall_2']*1) );
+	addS('Result_Wall_3',(sData[curDataSet]['b_Transmission_Wall_3']*1)+(sData[curDataSet]['A_Wall_3']*1)+(sData[curDataSet]['U_Refurbished_Wall_3']*1) );
+	addS('Result_Floor_1',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Floor_1']*1) );
+	addS('Result_Floor_2',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Floor_2']*1) );
+	addS('Result_Window_1',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Window_1']*1) );
+	addS('Result_Window_2',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Window_2']*1) );
+	addS('Result_Door_1',(sData[curDataSet]['b_Transmission_Roof_1']*1)+(sData[curDataSet]['A_Roof_1']*1)+(sData[curDataSet]['U_Refurbished_Door_1']*1) );
+
+	/***/	
 }
 
 function fillDataSet(){
